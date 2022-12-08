ba897b6509e390d8743b135f258840445491a0b0
brillout <github.com@brillout.com>
2022-12-08 22:04:48 +0000

    Deploying to gh-pages from @ brillout/vps-docs-cn@9256a1211c320a45d14c5453ab64fcd10ddaef14 🚀

---
+++ assets/chunk-490e38f3.js
@@ -0,0 +1,1 @@
+n();function n(){document.getElementById("npm-init-code-snippet").onclick=async()=>{window.navigator.clipboard&&await window.navigator.clipboard.writeText("npm init vite-plugin-ssr@latest");const t=document.getElementById("npm-init-code-snippet"),e="aria-label",i=t.getAttribute(e);t.setAttribute(e,"Copied"),setTimeout(()=>{t.setAttribute(e,i)},1200)}}
---
+++ assets/pages/react-tour.page.client.b98d4a65.js
@@ -0,0 +1,1 @@
+import"../chunk-490e38f3.js";
